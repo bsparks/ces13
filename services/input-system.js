8d00ffe05fa74876a24192c7fe0f9841f81ae729
Ben Sparks <bsparks@unicon.net>
2015-08-15 13:30:04 -0700

    working on inventory :video_game:

--- services/input-system.js
+++ services/input-system.js
@@ -1,43 +1,64 @@
 function InputSystem() {
     this._keys = {};
-    
+
     this.codes = {
         LEFT: 37,
-        RIGHT: 39
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40
     };
-    
+
     window.addEventListener('keydown', this.onKeyDown.bind(this));
     window.addEventListener('keyup', this.onKeyUp.bind(this));
 };
 
 InputSystem.prototype.onKeyDown = function(event) {
-    this._keys[event.keyCode] = true;
+    this._keys[event.keyCode] = this._keys[event.keyCode] || {};
+
+    if (!this._keys[event.keyCode].down) {
+      this._keys[event.keyCode].pressed = true;
+    }
+    this._keys[event.keyCode].down = true;
+    console.debug('keydown', this._keys[event.keyCode]);
 };
 
 InputSystem.prototype.onKeyUp = function(event) {
-    this._keys[event.keyCode] = false;
+    this._keys[event.keyCode] = this._keys[event.keyCode] || {};
+    this._keys[event.keyCode].down = false;
 };
 
 InputSystem.prototype._getKey = function(which) {
     var key;
-    
+
     if (typeof which === 'number') {
         key = this._keys[which];
     }
-    
+
     if (typeof which === 'string') {
         key = this._keys[this.codes[which]];
     }
-    
+
     return key;
 };
 
 InputSystem.prototype.pressed = function(which) {
     var key = this._getKey(which);
-    return !!key;
+    if (!key) {
+      return false;
+    }
+
+    if (key.pressed) {
+      key.pressed = false;
+      return true;
+    }
+
+    return false;
 };
 
 InputSystem.prototype.down = function(which) {
     var key = this._getKey(which);
-    return !!key;
+    if (!key) {
+      return false;
+    }
+    return key.down;
 };
--- systems/player-system.js
+++ systems/player-system.js
@@ -1,21 +1,29 @@
 function PlayerSystem(world) {
     this.world = world;
-    
+
     this.update = function(dt) {
         var players = this.world.getEntities('player', 'transform');
         var input = this.world.getSystem('InputSystem');
-        
+
         players.forEach(function(player) {
            var pc = player.getComponent('player'),
             transform = player.getComponent('transform');
-            
+
             if (input.pressed('LEFT')) {
                 //console.log('left: ', transform, player, dt);
                 transform.x -= pc.speed * dt;
             }
-            
+
             if (input.pressed('RIGHT')) {
                 transform.x += pc.speed * dt;
+            }
+
+            if (input.pressed('UP')) {
+                transform.y -= pc.speed * dt;
+            }
+
+            if (input.pressed('DOWN')) {
+                transform.y += pc.speed * dt;
             }
         });
     };
